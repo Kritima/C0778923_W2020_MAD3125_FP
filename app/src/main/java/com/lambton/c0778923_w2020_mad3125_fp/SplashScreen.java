396ceae2c8df355165c12c24dfa97c3fb605e9f5
Kritima Kukreja <kritimakukreja5@gmail.co>
2020-04-11 17:02:50 -0400

    [KRITIMA] Splash screen logic added

---
+++ app/src/main/java/com/lambton/c0778923_w2020_mad3125_fp/SplashScreen.java
@@ -0,0 +1,31 @@
+package com.lambton.c0778923_w2020_mad3125_fp;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    public static int SPLASH_TIME_OUT = 5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(SplashScreen.this, LoginActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+    }
+
