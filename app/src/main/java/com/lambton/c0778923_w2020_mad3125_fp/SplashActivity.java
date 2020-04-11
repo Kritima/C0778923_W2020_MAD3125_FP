6c3c793eb5a1f31e4f7656953182eb1015ab0ed8
Kritima Kukreja <kritimakukreja5@gmail.co>
2020-04-11 17:14:12 -0400

    [KRITIMA] Activity files renamed

--- app/src/main/java/com/lambton/c0778923_w2020_mad3125_fp/SplashActivity.java
+++ app/src/main/java/com/lambton/c0778923_w2020_mad3125_fp/SplashActivity.java
@@ -7,21 +7,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
-public class SplashScreen extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     public static int SPLASH_TIME_OUT = 5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+        setContentView(R.layout.activity_splash);
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent homeIntent = new Intent(SplashScreen.this, LoginActivity.class);
+                Intent homeIntent = new Intent(SplashActivity.this, LoginActivity.class);
                 startActivity(homeIntent);
                 finish();
             }
