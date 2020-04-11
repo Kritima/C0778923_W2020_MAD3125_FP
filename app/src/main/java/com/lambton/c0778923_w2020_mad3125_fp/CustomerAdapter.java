b417823868ff36afc0b9105c72f964c09185fab2
Kritima Kukreja <kritimakukreja5@gmail.co>
2020-04-11 18:16:15 -0400

    [KRITIMA] Customer Adapter constructor added

--- app/src/main/java/com/lambton/c0778923_w2020_mad3125_fp/CustomerAdapter.java
+++ app/src/main/java/com/lambton/c0778923_w2020_mad3125_fp/CustomerAdapter.java
@@ -1,4 +1,25 @@
 package com.lambton.c0778923_w2020_mad3125_fp;
 
-public class CustomerAdapter {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class CustomerAdapter extends RecyclerView.Adapter<CustomerAdapter.CustomerViewHolder> {
+
+        private ArrayList<String> customerArrayList;
+
+        public CustomerAdapter(ArrayList<String> customerArray)
+        {
+            this.customerArrayList = customerArray;
+        }
+        
 }
