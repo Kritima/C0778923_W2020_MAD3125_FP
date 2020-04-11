4886ac620c31442f0988a4e127fbc519fc8b3cff
Kritima Kukreja <kritimakukreja5@gmail.co>
2020-04-11 19:04:36 -0400

    [KRITIMA] Customer variables declared in Customer class

---
+++ app/src/main/java/com/lambton/c0778923_w2020_mad3125_fp/Customer.java
@@ -0,0 +1,12 @@
+package com.lambton.c0778923_w2020_mad3125_fp;
+
+public class Customer {
+
+    private String id;
+    private String fName;
+    private String lName;
+    private String email;
+
+    
+
+}
