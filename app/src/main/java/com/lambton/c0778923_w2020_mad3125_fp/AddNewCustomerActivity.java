0ddefd374d198d2f9b90a6d350514a69d2934e2b
Kritima Kukreja <kritimakukreja5@gmail.co>
2020-04-12 12:44:28 -0400

    [KRITIMA] Add New Customer new screen added

---
+++ app/src/main/java/com/lambton/c0778923_w2020_mad3125_fp/AddNewCustomerActivity.java
@@ -0,0 +1,14 @@
+package com.lambton.c0778923_w2020_mad3125_fp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddNewCustomerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_customer);
+    }
+}
