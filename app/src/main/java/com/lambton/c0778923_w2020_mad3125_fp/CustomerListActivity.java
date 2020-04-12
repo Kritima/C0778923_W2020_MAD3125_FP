abe2b6d6144e023b6bc66cf4d0881d4d065a85c6
Kritima Kukreja <kritimakukreja5@gmail.co>
2020-04-12 12:36:35 -0400

    [KRITIMA] Populate customer list function added in Customer List Activity

--- app/src/main/java/com/lambton/c0778923_w2020_mad3125_fp/CustomerListActivity.java
+++ app/src/main/java/com/lambton/c0778923_w2020_mad3125_fp/CustomerListActivity.java
@@ -19,41 +19,24 @@ public class CustomerListActivity extends AppCompatActivity {
     private ArrayList<Customer> customerListArrayList;
     private CustomerAdapter customerAdapter;
 
-    JSONParser parser = new JSONParser();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_list);
 
         rvCustomerList = findViewById(R.id.recyclerviewCustomers);
-        populateList();
+        populateCustomer();
 
         customerAdapter = new CustomerAdapter(customerListArrayList);
         rvCustomerList.setLayoutManager(new LinearLayoutManager(this));
         rvCustomerList.setAdapter(customerAdapter);
 
-        try {
-            Object obj = parser.parse(new FileReader("/assets/customer.json"));
-
-            // A JSON object. Key value pairs are unordered. JSONObject supports java.util.Map interface.
-            JSONObject jsonObject = (JSONObject) obj;
-
-            // A JSON array. JSONObject supports java.util.List interface.
-            JSONArray companyList = (JSONArray) jsonObject.get("Company List");
-
-            // An iterator over a collection. Iterator takes the place of Enumeration in the Java Collections Framework.
-            // Iterators differ from enumerations in two ways:
-            // 1. Iterators allow the caller to remove elements from the underlying collection during the iteration with well-defined semantics.
-            // 2. Method names have been improved.
-            Iterator<JSONObject> iterator = companyList.iterator();
-            while (iterator.hasNext()) {
-                System.out.println(iterator.next());
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
 
     }
+
+    private void populateCustomer() {
+        customerListArrayList = new ArrayList<>();
+
+        customerListArrayList.add(new Customer("C004", "Kritima", "Kukreja", "kri@gmail.com"));
+    }
+}
