e60f89b09c4a41d4e9c0b2966f16f00efef2f333
Kritima Kukreja <kritimakukreja5@gmail.co>
2020-04-14 15:15:14 -0400

    [KRITIMA] Recycler view set in Show Bill Details activity

--- app/src/main/java/com/lambton/c0778923_w2020_mad3125_fp/ShowBillDetailsActivity.java
+++ app/src/main/java/com/lambton/c0778923_w2020_mad3125_fp/ShowBillDetailsActivity.java
@@ -1,6 +1,8 @@
 package com.lambton.c0778923_w2020_mad3125_fp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -10,14 +12,22 @@ import java.util.ArrayList;
 public class ShowBillDetailsActivity extends AppCompatActivity {
 
     private RecyclerView rvBillItemList;
-    private ArrayList<BillItem> BillItemListArrayList;
+    private ArrayList<BillItem> billItemListArrayList;
     private BillItemAdapter billItemAdapter;
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_bill_details);
+
+        rvBillItemList = findViewById(R.id.recyclerviewBillItem);
+        billItemAdapter = new BillItemAdapter(billItemListArrayList);
+        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(this, 2);
+        rvBillItemList.setLayoutManager(mLayoutManager);
+        rvBillItemList.setAdapter(billItemAdapter);
     }
+
 
 
 }
