0a4e72f6406a8306d9a0a37bcc093381c2c0a1a0
Kritima Kukreja <kritimakukreja5@gmail.co>
2020-04-15 13:12:56 -0400

    [KRITIMA] Mobile Class added

---
+++ app/src/main/java/com/lambton/c0778923_w2020_mad3125_fp/models/Mobile.java
@@ -0,0 +1,12 @@
+package com.lambton.c0778923_w2020_mad3125_fp.models;
+
+public class Mobile {
+
+    private String mobileModelName;
+    private String planName;
+    private String mobileNumber;
+    private int internetGbUsed;
+    private int minuteUsed;
+
+    
+}
