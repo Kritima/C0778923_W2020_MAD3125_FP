d13eb76269599fc2d0b9ab09221258032cbb6a2b
Kritima Kukreja <kritimakukreja5@gmail.co>
2020-04-18 12:43:47 -0400

    [KRITIMA] Strings added for add new customer xml

--- app/src/main/java/com/lambton/c0778923_w2020_mad3125_fp/ui/AddNewCustomerActivity.java
+++ app/src/main/java/com/lambton/c0778923_w2020_mad3125_fp/ui/AddNewCustomerActivity.java
@@ -5,12 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.lambton.c0778923_w2020_mad3125_fp.R;
+import com.lambton.c0778923_w2020_mad3125_fp.adapters.CustomerAdapter;
+import com.lambton.c0778923_w2020_mad3125_fp.models.Customer;
+
+import java.util.ArrayList;
 
 public class AddNewCustomerActivity extends AppCompatActivity {
+
+    private ArrayList<Customer> customerNewListArrayList;
+    Customer c = new Customer();
+    private CustomerAdapter customerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_customer);
+
+
     }
 }
