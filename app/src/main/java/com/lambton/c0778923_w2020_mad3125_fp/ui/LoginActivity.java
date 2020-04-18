5c82ef02b879270c8cc10d21cf215189af800781
Kritima Kukreja <kritimakukreja5@gmail.co>
2020-04-18 11:43:06 -0400

    [KRITIMA] Shared Preference added in login activity

--- app/src/main/java/com/lambton/c0778923_w2020_mad3125_fp/ui/LoginActivity.java
+++ app/src/main/java/com/lambton/c0778923_w2020_mad3125_fp/ui/LoginActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -23,6 +24,9 @@ public class LoginActivity extends AppCompatActivity {
     CheckBox rememberMe;
     AlertDialog.Builder builder;
 
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +36,9 @@ public class LoginActivity extends AppCompatActivity {
         email = findViewById(R.id.editTextEmail);
         password = findViewById(R.id.editTextPassword);
         rememberMe = findViewById(R.id.checkBoxRemember);
+
+        sharedPreferences=getSharedPreferences("LoginPrefs", MODE_PRIVATE);
+        editor=sharedPreferences.edit();
     }
 }
 
