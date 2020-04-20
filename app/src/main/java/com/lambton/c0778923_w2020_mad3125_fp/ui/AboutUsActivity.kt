8acc772c10fb753a1a095a3a980ea4ad0108cdc8
Kritima Kukreja <kritimakukreja5@gmail.co>
2020-04-20 09:14:09 -0400

    [KRITIMA] Web view set in About us activity

--- app/src/main/java/com/lambton/c0778923_w2020_mad3125_fp/ui/AboutUsActivity.kt
+++ app/src/main/java/com/lambton/c0778923_w2020_mad3125_fp/ui/AboutUsActivity.kt
@@ -8,4 +8,28 @@ import com.lambton.c0778923_w2020_mad3125_fp.R
 import java.io.IOException
 import java.nio.charset.StandardCharsets
 
-class AboutUsActivity :
+class AboutUsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about_us)
+
+        val myWebView: WebView = findViewById(R.id.webViewAboutUs)
+
+        val mActionBar = supportActionBar
+        mActionBar!!.hide()
+        val assetManager = this.assets
+
+        try {
+            val inputStream = assetManager.open("aboutus.html")
+            val size = inputStream.available()
+            val buffer = ByteArray(size)
+            inputStream.read(buffer)
+            inputStream.close()
+            val content = String(buffer, StandardCharsets.UTF_8)
+            Log.d("DATA", content)
+            myWebView?.loadData(content, "text/html", "utf-8")
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+}
