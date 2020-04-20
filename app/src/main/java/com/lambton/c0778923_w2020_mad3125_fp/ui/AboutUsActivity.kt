e4ab20df30f65f9172c7b30c75db53df4c385741
Kritima Kukreja <kritimakukreja5@gmail.co>
2020-04-20 09:13:29 -0400

    [KRITIMA] Web view added in about us xml

--- app/src/main/java/com/lambton/c0778923_w2020_mad3125_fp/ui/AboutUsActivity.kt
+++ app/src/main/java/com/lambton/c0778923_w2020_mad3125_fp/ui/AboutUsActivity.kt
@@ -1,4 +1,11 @@
 package com.lambton.c0778923_w2020_mad3125_fp.ui
 
-class AboutUsActivity {
-}
+import android.os.Bundle
+import android.util.Log
+import android.webkit.WebView
+import androidx.appcompat.app.AppCompatActivity
+import com.lambton.c0778923_w2020_mad3125_fp.R
+import java.io.IOException
+import java.nio.charset.StandardCharsets
+
+class AboutUsActivity :
